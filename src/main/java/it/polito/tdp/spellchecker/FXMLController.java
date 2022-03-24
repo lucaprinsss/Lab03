a5de7088f4874fcaa93584a000778c1e467ba0bb
luca <luca@172.22.33.37>
2022-03-24 08:52:10 +0100

    Da rivedere il funzionamento della label che indica gli errori, in particolare quando non si inserice il testo. Manca da inserire il tempo impiegato e (eventualmente) il punto 2.

--- src/main/java/it/polito/tdp/spellchecker/FXMLController.java
+++ src/main/java/it/polito/tdp/spellchecker/FXMLController.java
@@ -5,9 +5,12 @@
 package it.polito.tdp.spellchecker;
 
 import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import it.polito.tdp.spellchecker.model.Modello;
+import it.polito.tdp.spellchecker.model.RichWord;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -17,7 +20,7 @@ import javafx.scene.control.TextArea;
 
 public class FXMLController {
 	
-	private Modello model;
+	private Modello modello;
 
     @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
@@ -32,7 +35,7 @@ public class FXMLController {
     private Button btnClear; // Value injected by FXMLLoader
 
     @FXML // fx:id="cmbLanguage"
-    private ComboBox<?> cmbLanguage; // Value injected by FXMLLoader
+    private ComboBox<String> cmbLanguage; // Value injected by FXMLLoader
 
     @FXML // fx:id="lblError"
     private Label lblError; // Value injected by FXMLLoader
@@ -48,12 +51,49 @@ public class FXMLController {
 
     @FXML
     void doCheck(ActionEvent event) {
-
+    	String testo=txtText.getText();
+    	
+    	testo.replaceAll("[.,\\/#!$%\\^&\\*;:{}=\\-_`~()\\[\\]\"]","");
+    	String testoMin=testo.toLowerCase();
+    	
+    	String array[]=testoMin.split(" ");
+    	
+    	if(array.length==0) {
+    		lblError.setText("Inserire testo");
+    		return;
+    	}
+    		
+    	List<String> lista=new LinkedList<String>();
+    	for(String s:array) {
+    		lista.add(s);
+    	}
+    	
+    	String lingua=cmbLanguage.getValue();
+    	modello.loadDictionary(lingua);
+    	
+    	List<RichWord> listaCorretta=new LinkedList<RichWord>();
+    	listaCorretta=modello.spellCheckText(lista);
+    	
+    	if(listaCorretta.isEmpty()) {
+    		lblError.setText("Non ci sono errori");
+    		return;
+    	}
+    	
+    	Integer cont=0;
+    	txtWrongWord.clear();
+    	for(RichWord r:listaCorretta)
+    		if(!r.isCorretta()) {
+    			txtWrongWord.appendText(r.getParola()+"\n");
+    			cont++;
+    		}
+    	
+    	lblError.setText("Sono presenti "+cont+" errori!");
     }
 
     @FXML
     void doClear(ActionEvent event) {
-
+    	txtText.clear();
+    	txtWrongWord.clear();
     }
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
@@ -65,11 +105,15 @@ public class FXMLController {
         assert lblTime != null : "fx:id=\"lblTime\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtText != null : "fx:id=\"txtText\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtWrongWord != null : "fx:id=\"txtWrongWord\" was not injected: check your FXML file 'Scene.fxml'.";
-
+        
+        cmbLanguage.getItems().clear();
+        cmbLanguage.getItems().add("English");
+        cmbLanguage.getItems().add("Italian");
+        
     }
     
     public void setModel(Modello model) {
-    	this.model=model;
+    	this.modello=model;
     }
 
 }
